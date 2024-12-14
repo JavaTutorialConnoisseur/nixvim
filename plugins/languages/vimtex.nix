ac0ca6e4c06d56b4d3893e3955063410c0aa8e87
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-14 01:05:11 +0100

    Fixed the ]] being annoying in latex files

--- plugins/languages/vimtex.nix
+++ plugins/languages/vimtex.nix
@@ -3,6 +3,7 @@
 {
   opts = {
     enable = true;
+    texlivePackage = pkgs.texliveFull;
     settings = {
       compiler_method = "latexmk -pdf";
       view_general_viewer = "okular";
@@ -10,8 +11,51 @@
     };
   };
 
-  # TODO: add / label keybinds for compilation?
+  rootOpts.autoCmd = [{
+    command.__raw = ''
+      vim.cmd(":iunmap <silent><buffer><expr> ]]")
+    '';
 
-  rootOpts.extraPackages = [ pkgs.texliveBasic ];
+    event = [ "FileType" ];
+    pattern = [ "tex" "latex" ];
+    desc = "Remove the annoying ]] binding";
+  }];
+
+  rootOpts.extraPackages = [ pkgs.texlivePackages.latexmk ];
   rootOpts.plugins.transparent.settings.extra_groups = [ "VimtexMsg" ];
+
+  # TODO: add / label keybinds for compilation?
+  #   -> maybe add a lualine feature or something?
+  # Motions (link to GIF demonstrations)
+  #
+  #     Move between section boundaries with [[, [], ][, and ]]
+  #     Move between environment boundaries with [m, [M, ]m, and ]M
+  #     Move between math environment boundaries with [n, [N, ]n, and ]N
+  #     Move between frame environment boundaries with [r, [R, ]r, and ]R
+  #     Move between comment boundaries with [* and ]*
+  #     Move between matching delimiters with %
+  #
+  # Text objects (link to GIF demonstrations)
+  #
+  #     ic ac Commands
+  #     id ad Delimiters
+  #     ie ae LaTeX environments
+  #     i$ a$ Math environments
+  #     iP aP Sections
+  #     im am Items
+  #
+  # Other mappings (link to GIF demonstrations)
+  #
+  #     Delete the surrounding command, environment or delimiter with dsc/dse/ds$/dsd
+  #     Change the surrounding command, environment or delimiter with csc/cse/cs$/csd
+  #     Toggle starred command or environment with tsc/tse
+  #     Toggle inline and displaymath with ts$
+  #     Toggle between e.g. () and \left(\right) with tsd
+  #     Toggle (inline) fractions with tsf
+  #     Toggle line-break macro \\ with tsb
+  #     Close the current environment/delimiter in insert mode with ]]
+  #     Add \left ... \right) modifiers to surrounding delimiters with <F8>
+  #     Insert new command with <F7>
+  #     Convenient insert mode mappings for faster typing of e.g. maths
+  #     Context menu on citations (e.g. \cite{...}) mapped to <cr>
 }
