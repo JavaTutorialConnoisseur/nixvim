86ef9ee0d712ea1c552e15ca12e20f56de102af8
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-14 17:24:17 +0100

    fix(plugins.languages): autoCmd command.__raw was fucked

--- plugins/languages/haskell.nix
+++ plugins/languages/haskell.nix
@@ -7,11 +7,13 @@ _:
   };
 
   rootOpts.autoCmd = [{
-    command.__raw = ''
-      vim.cmd [[
-        setlocal tabstop=2
-        setlocal shiftwidth=2
-      ]]
+    callback.__raw = ''
+      function()
+        vim.cmd [[
+          setlocal tabstop=2
+          setlocal shiftwidth=2
+        ]]
+      end
     '';
     event = [ "FileType" ];
     pattern = [ "haskell" ];
