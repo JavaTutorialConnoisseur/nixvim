90a2aec0b3e4fc34e909958359e26f10a72a4fa1
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-17 21:34:12 +0100

    Finished codesnap

---
+++ plugins/coding/codesnap.nix
@@ -0,0 +1,46 @@
+_:
+
+{
+  opts = {
+    enable = true;
+    settings = {
+      breadcrumbs_separator = "/";
+
+      has_breadcrumbs = true;
+      has_line_number = true;
+      mac_window_bar = true;
+      min_width = 40;
+
+      bg_x_padding = 20;
+      bg_y_padding = 20;
+      bg_theme = "grape";
+
+      save_path = "~/Pictures/Screenshots/";
+      title = "";
+      watermark = "";
+    };
+  };
+
+  rootOpts.keymaps = [
+    {
+      mode = "x";
+      key = "<leader>pc";
+      options.desc = "Pretty-print to clipboard";
+      action = "<cmd>CodeSnap<cr>";
+    }
+
+    {
+      mode = "x";
+      key = "<leader>ps";
+      options.desc = "Pretty-print to ~/Pictures/Screenshots";
+      action = "<cmd>CodeSnapSave<cr>";
+    }
+
+    {
+      mode = "x";
+      key = "<leader>ph";
+      options.desc = "Pretty-print highlighted to ~/Pictures/Screenshots";
+      action = "<cmd>CodeSnapSaveHighlight<cr>";
+    }
+  ];
+}
