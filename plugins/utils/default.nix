76f02b4040c4e181ae4fc26c466574f82b767884
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-18 20:00:29 +0100

    refactor(plugins): split categories for 'ui' and 'ui'-adjacent plugins

---
+++ plugins/utils/default.nix
@@ -0,0 +1,18 @@
+{ lib, ... }@args:
+
+let
+  definitions = lib.attrNames (lib.filterAttrs (filename: kind:
+    filename != "default.nix" && (kind == "regular" || kind == "directory"))
+    (builtins.readDir ./.));
+in lib.mkMerge (map (file:
+  let
+    pluginName = lib.elemAt (lib.splitString "." file) 0;
+    plugin = import ./${file} args;
+  in lib.mkMerge [
+    (lib.optionalAttrs (plugin ? opts) { plugins.${pluginName} = plugin.opts; })
+    (lib.optionalAttrs (plugin ? extra) {
+      extraConfigLua = plugin.extra.config or "";
+      extraPlugins = plugin.extra.packages;
+    })
+    (plugin.rootOpts or { })
+  ]) definitions)
