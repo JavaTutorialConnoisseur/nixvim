14978b7b3f32b0d77d1c6466cb9b5f833fec5cdc
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-19 00:23:54 +0100

    refactor: default.nix recusive importing fully implemented

--- plugins/lsp/default.nix
+++ plugins/lsp/default.nix
@@ -1,25 +1,7 @@
 { lib, ... }@args:
 
-let
-  withLSP = args.withLSP or true;
-
-  disabled_files = [ "haskell.nix" "scala.nix" ];
-  filterDisabled = dir:
-    lib.filterAttrs (filename: _: !(lib.elem filename disabled_files)) dir;
-
-  definitions = lib.attrNames (filterDisabled (lib.filterAttrs (filename: kind:
-    filename != "default.nix" && (kind == "regular" || kind == "directory")
-    && (if filename == "lsp.nix" then withLSP else true))
-    (builtins.readDir ./.)));
-in lib.mkMerge (map (file:
-  let
-    pluginName = lib.elemAt (lib.splitString "." file) 0;
-    plugin = import ./${file} args;
-  in lib.mkMerge [
-    (lib.optionalAttrs (plugin ? opts) { plugins.${pluginName} = plugin.opts; })
-    (lib.optionalAttrs (plugin ? extra) {
-      extraConfigLua = plugin.extra.config or "";
-      extraPlugins = plugin.extra.packages;
-    })
-    (plugin.rootOpts or { })
-  ]) definitions)
+args.defaultFn {
+  lib = lib;
+  dir = ./.;
+  args = args;
+}
--- plugins/projects/default.nix
+++ plugins/projects/default.nix
@@ -3,19 +3,8 @@
 # All plugins that don't directly EDIT, but manage projects and project-related
 # things go here.
 
-let
-  definitions = lib.attrNames (lib.filterAttrs (filename: kind:
-    filename != "default.nix" && (kind == "regular" || kind == "directory"))
-    (builtins.readDir ./.));
-in lib.mkMerge (map (file:
-  let
-    pluginName = lib.elemAt (lib.splitString "." file) 0;
-    plugin = import ./${file} args;
-  in lib.mkMerge [
-    (lib.optionalAttrs (plugin ? opts) { plugins.${pluginName} = plugin.opts; })
-    (lib.optionalAttrs (plugin ? extra) {
-      extraConfigLua = plugin.extra.config or "";
-      extraPlugins = plugin.extra.packages;
-    })
-    (plugin.rootOpts or { })
-  ]) definitions)
+args.defaultFn {
+  lib = lib;
+  dir = ./.;
+  args = args;
+}
