f0136f15932c5fe70b89fff3bd41fcde4442a2ef
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-23 04:28:03 +0100

    Basic CMP-DAP for CC and Java (WIP)

--- plugins/languages/cc.nix
+++ plugins/languages/cc.nix
@@ -5,52 +5,38 @@ in {
   rootOpts.plugins.lsp.servers.ccls.enable = true;
 
   rootOpts.plugins.dap = {
-    adapters = {
-      executables.gdb = {
-        command = "gdb";
-        args =
-          [ "--interpreter=dap" "--eval-command" "-enable-pretty-printing" ];
-      };
-      executables.cc = { # BUG: not working
-        command = "cc";
-
-        args = [
-          "-g"
-          "-o"
-          "\${fileDirname}/\${fileBasenameNoExtension}"
-        ];
-      };
+    adapters.executables.gdb = {
+      command = "gdb";
+      args = [ "--interpreter=dap" ''--eval-command="set print pretty on"'' ];
     };
 
     configurations = {
-      c = [
-        {
-          name = "Debug (gdb)";
-          type = "gdb";
-          program = "\${fileBasenameNoExtension}";
-          request = "launch";
-        }
+      c = [{
+        name = "Launch";
+        type = "gdb";
+        request = "launch";
+        program.__raw = ''
+          function()
+            return vim.fn.input('Path to executable: ',
+              vim.fn.getcwd() .. '/', 'file')
+          end
+        '';
+        args.__raw = ''
+          function()
+            local inp = vim.fn.input('Input arguments: ')
 
-        {
-          name = "Compile (cc)";
-          type = "cc";
-          program = "\${file}";
-          request = "launch";
-        }
+            local gdbArgs = {}
+            for word in string.gmatch(inp, "%S+") do
+              table.insert(gdbArgs, word)
+            end
 
-        {
-          name = "Make (all)";
-          type = "shell";
-          command = "make";
-          args = [ "all" ];
-          cwd = "\${workspaceFolder}";
-          request = "launch";
-        }
-      ];
+            return gdbArgs
+          end
+        '';
+        cwd = "\${workspaceFolder}";
+        stopAtBeginningOfMainSubprogram = true;
+      }];
     };
-
-    # extensions.dap-???.enable = true;
-    # # whatever extension is useful for C/C++/C#
   };
 
   rootOpts.extraConfigLua = ''
@@ -60,6 +46,61 @@ in {
       vim.g.c_syntax_for_h = 0
     end
   '';
+
+  rootOpts.userCommands = {
+    Make = {
+      command.__raw = ''
+        function()
+          local result = vim.fn.system("make all")
+
+          if vim.v.shell_error ~= 0 then
+              print("Make (all) failed: " .. result)
+          end
+        end
+      '';
+      desc = "Try running 'make all'";
+    };
+    CC = {
+      nargs = "?";
+      command.__raw = ''
+        function(opts)
+          local filename = vim.fn.expand('%:t')
+          local filepath = vim.fn.expand('%:p')
+
+          local extension = vim.fn.fnamemodify(filename, ':e')
+
+          local valid_extensions = { 'c', 'o', 's', 'S' }
+          local is_valid = false
+          for _, ext in ipairs(valid_extensions) do
+              if extension == ext then
+                  is_valid = true
+                  break
+              end
+          end
+
+          if not is_valid then
+              print("Error: Invalid file extension.")
+              return
+          end
+
+          local debug_flag = ""
+          if opts.fargs[1] == "debug" then
+              debug_flag = "-g"
+          end
+
+          local output_filepath = vim.fn.fnamemodify(filepath, ':r')
+          local command = string.format("cc %s -o %s %s",
+            debug_flag, output_filepath, filepath)
+          local result = vim.fn.system(command)
+
+          if vim.v.shell_error ~= 0 then
+              print("Compilation failed: " .. result)
+          end
+        end
+      '';
+      desc = "Compile current file using 'cc'";
+    };
+  };
 
   rootOpts.autoCmd = [{
     callback.__raw = ''
--- plugins/languages/java.nix
+++ plugins/languages/java.nix
@@ -1,11 +1,22 @@
-_:
+{ pkgs, lib, ... }:
 
-{
-  rootOpts.plugins.nvim-jdtls = {
-    enable = true;
-    data = "~/Downloads"; # WARN: placeholder
-    configuration = "~/.cache/jdtls/config";
+let
+  start-jdt-server = lib.getExe (pkgs.writeShellScriptBin "start-jdt-server"
+    "jdtls -data ${workspacePath}/data_structures");
+  workspacePath = "/home/parrycat/Documents/projects/java/projects/workspaces";
+in {
+  rootOpts.plugins = {
+    nvim-jdtls = {
+      enable = true;
+      data = "../.";
+      # for now: hardcoded. Tries to fix indexing issue
+      settings.java.gradle.enabled = true;
+    };
+
+    # lsp.servers.java_language_server = {
+    #   enable = true;
+    #   cmd = [ "${start-jdt-server}" ];
+    #   package = pkgs.jdt-language-server;
+    # };
   };
-
-  rootOpts.plugins.lsp.servers.jdtls.enable = true;
 }
