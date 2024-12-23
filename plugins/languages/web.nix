b4a9e2ee04ef764d3f65c87f87af56094eb2b343
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-23 15:25:33 +0100

    Added web (js/css/html/ts) support for LSP

---
+++ plugins/languages/web.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  rootOpts.plugins.lsp.servers = {
+    cssls.enable = true;
+    html.enable = true;
+    ts_ls.enable = true;
+  };
+
+  rootOpts.extraPackages = [ pkgs.vscode-langservers-extracted ];
+}
