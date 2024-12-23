f0e455615b0c58a2385a843211f4fb1500b3571f
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-23 14:43:21 +0100

    Added colorizer (css and # code hl) plugin

---
+++ plugins/style/colorizer.nix
@@ -0,0 +1,13 @@
+_:
+
+{
+  opts = {
+    enable = true;
+    settings.filetypes = {
+      __unkeyed-1 = "*";
+      __unkeyed-2 = "!vim";
+      css = { rgb_fn = true; };
+      html = { names = false; };
+    };
+  };
+}
