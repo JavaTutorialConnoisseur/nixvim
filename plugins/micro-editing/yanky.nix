668be88c6f2380e3f86bab078e69831e847f7b5a
JavaTutorialConnoisseur <parryfeline@proton.me>
2024-12-23 04:27:34 +0100

    feat(plugins): TelescopeWithTheme instead of Telescope for some plugins

--- plugins/micro-editing/yanky.nix
+++ plugins/micro-editing/yanky.nix
@@ -9,7 +9,11 @@ _:
   rootOpts.keymaps = [{
     mode = "n";
     key = "<leader>gy";
-    action = "<cmd>Telescope yank_history<cr>";
+    action.__raw = ''
+      function()
+        TelescopeWithTheme('yank_history', { }, 'yank_history')
+      end
+    '';
     options.desc = "Yank selections";
   }];
 }
