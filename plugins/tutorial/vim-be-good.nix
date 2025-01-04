7684d8c3603b4b1dd5cbfdd9b1c5cb6379754b86
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-01-05 00:06:49 +0100

    feat(plugins.tutorial): keybind for vim-be-good

--- plugins/tutorial/vim-be-good.nix
+++ plugins/tutorial/vim-be-good.nix
@@ -2,4 +2,11 @@ _:
 
 {
   opts.enable = true;
+
+  rootOpts.keymaps = [{
+    mode = "n";
+    key = "<leader>lv";
+    options.desc = "Start vim-be-good to get good";
+    action = "<cmd>VimBeGood<cr>";
+  }];
 }
