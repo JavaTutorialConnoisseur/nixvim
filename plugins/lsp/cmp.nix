7a9a5d1794c44a0c28f88090b7d73d20400e968f
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-01-26 16:46:43 +0100

    Added jump methods for luasnip snippets

--- plugins/lsp/cmp.nix
+++ plugins/lsp/cmp.nix
@@ -23,6 +23,10 @@ _:
         "<CR>" = "cmp.mapping.confirm({ select = true })";
         "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
         "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+        "<C-l>" =
+          "cmp.mapping(function() if require('luasnip').jumpable(1) then require('luasnip').jump(1) end end, { 'i', 's' })";
+        "<C-h>" =
+          "cmp.mapping(function() if require('luasnip').jumpable(-1) then require('luasnip').jump(-1) end end, { 'i', 's' })";
       };
     };
   };
@@ -33,6 +37,33 @@ _:
     friendly-snippets.enable = true;
     intellitab.enable = true;
   };
+
+  rootOpts.plugins.which-key.settings.spec = [
+    {
+      __unkeyed-1 = "<leader>s";
+      group = "Snippets";
+      icon = {
+        icon = " ";
+        color = "purple";
+      };
+    }
+
+    {
+      __unkeyed-1 = "<leader>sn";
+      __unkeyed-2 =
+        "<cmd>lua if require('luasnip').jumpable(1) then require('luasnip').jump(1) end<cr>";
+      desc = "Jump to next snippet chunk";
+      mode = "n";
+    }
+
+    {
+      __unkeyed-1 = "<leader>sp";
+      __unkeyed-2 =
+        "<cmd>lua if require('luasnip').jumpable(-1) then require('luasnip').jump(-1) end<cr>";
+      desc = "Jump to previous snippet chunk";
+      mode = "n";
+    }
+  ];
 
   rootOpts.keymaps = [{
     mode = "i";
