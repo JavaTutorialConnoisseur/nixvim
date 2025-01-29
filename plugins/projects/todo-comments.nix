d45541f6416df4436f3485e0c144c5ed21df840c
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-01-29 20:04:42 +0100

    Added TODO jumping keybinds and new keywords

--- plugins/projects/todo-comments.nix
+++ plugins/projects/todo-comments.nix
@@ -3,22 +3,56 @@ _:
 {
   opts = {
     enable = true;
-    settings.keywords.FUTURE = {
-      color = "hint";
-      icon = "󱄅 ";
+    settings.keywords = {
+      FUTURE = {
+        color = "hint";
+        icon = "󱄅 ";
+      };
+
+      IMPL = {
+        color = "info";
+        icon = " ";
+        alt = [ "ADDME" ];
+      };
     };
   };
 
-  rootOpts.keymaps = [{
-    mode = "n";
-    key = "<leader>td";
-    options.desc = "Find TODO's";
-    action.__raw = ''
-      function()
-        TelescopeWithTheme('todo', { }, 'todo-comments')
-      end
-    '';
-  }];
+  rootOpts.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>td";
+      options.desc = "Find TODO's";
+      action.__raw = ''
+        function()
+          TelescopeWithTheme('todo', { }, 'todo-comments')
+        end
+      '';
+    }
+
+    {
+      mode = "n";
+      key = "[t";
+      action.__raw = ''
+        function()
+            require("todo-comments").jump_prev({
+              keywords = { "TODO", "ADDME", "IMPL" }
+            })
+        end
+      '';
+    }
+
+    {
+      mode = "n";
+      key = "]t";
+      action.__raw = ''
+        function()
+            require("todo-comments").jump_next({
+              keywords = { "TODO", "ADDME", "IMPL" }
+            })
+        end
+      '';
+    }
+  ];
 
   rootOpts.plugins.transparent.settings.extra_groups = [
     "TodoSignNOTE"
