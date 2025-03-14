94c12ae10494e7addd7a2e3ab6254a5b5340c76a
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-03-14 18:10:59 +0100

    fix(flake): cross-config works, flake lock update

--- flake.nix
+++ flake.nix
@@ -26,47 +26,52 @@
         replace = "#1c1408";
       };
     in {
-      packages = { theme ? defaultTheme, disabled ? defaultDisabled }:
-        forAllSystems (system:
-          let
-            pkgs = import nixpkgs { inherit system; };
+      packages = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
 
-            disabledLangs = langs: map (lang: lang + ".nix") langs;
+          disabledLangs = langs: map (lang: lang + ".nix") langs;
 
-            mkNixvim = theme: specialArgs:
-              nixvim.legacyPackages.${system}.makeNixvimWithModule {
-                inherit pkgs;
-                extraSpecialArgs = specialArgs // { inherit pkgs; } // theme;
-                module = ./.;
-              };
+          mkNixvim = theme: specialArgs:
+            nixvim.legacyPackages.${system}.makeNixvimWithModule {
+              inherit pkgs;
+              extraSpecialArgs = specialArgs // { inherit pkgs; } // theme;
+              module = ./.;
+            };
 
-            defaultFn = { lib, dir, args, ignore ? [ ] }:
+          defaultFn = { lib, dir, args, ignore ? [ ] }:
+            let
+              shouldRemain = x: !(builtins.elem x ignore);
+              definitions = lib.filter shouldRemain (lib.attrNames
+                (lib.filterAttrs (filename: kind:
+                  filename != "default.nix"
+                  && (kind == "regular" || kind == "directory"))
+                  (builtins.readDir dir)));
+            in lib.mkMerge (map (file:
               let
-                shouldRemain = x: !(builtins.elem x ignore);
-                definitions = lib.filter shouldRemain (lib.attrNames
-                  (lib.filterAttrs (filename: kind:
-                    filename != "default.nix"
-                    && (kind == "regular" || kind == "directory"))
-                    (builtins.readDir dir)));
-              in lib.mkMerge (map (file:
-                let
-                  pluginName = lib.elemAt (lib.splitString "." file) 0;
-                  plugin = import (dir + "/${file}") args;
-                in lib.mkMerge [
-                  (lib.optionalAttrs (plugin ? opts) {
-                    plugins.${pluginName} = plugin.opts;
-                  })
-                  (lib.optionalAttrs (plugin ? extra) {
-                    extraConfigLua = plugin.extra.config or "";
-                    extraPlugins = plugin.extra.packages;
-                  })
-                  (plugin.rootOpts or { })
-                ]) definitions);
-          in {
-            default = mkNixvim theme {
+                pluginName = lib.elemAt (lib.splitString "." file) 0;
+                plugin = import (dir + "/${file}") args;
+              in lib.mkMerge [
+                (lib.optionalAttrs (plugin ? opts) {
+                  plugins.${pluginName} = plugin.opts;
+                })
+                (lib.optionalAttrs (plugin ? extra) {
+                  extraConfigLua = plugin.extra.config or "";
+                  extraPlugins = plugin.extra.packages;
+                })
+                (plugin.rootOpts or { })
+              ]) definitions);
+        in {
+          default = mkNixvim defaultTheme {
+            disabledLangs = (disabledLangs defaultDisabled);
+            defaultFn = defaultFn;
+          };
+
+          configurable = { theme ? defaultTheme, disabled ? defaultDisabled }:
+            mkNixvim theme {
               disabledLangs = (disabledLangs disabled);
               defaultFn = defaultFn;
             };
-          });
+        });
     };
 }
