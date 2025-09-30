c67f6510dd942af517d6e65dcbbd6875177f5bb1
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-09-30 18:53:14 +0200

    chore(plugins.style): comment update

--- plugins/style/transparent.nix
+++ plugins/style/transparent.nix
@@ -1,6 +1,8 @@
 _: {
   opts = {
     enable = true;
+    # TODO: see if any more groups need to be added here.
+    # (Just run 'highlight')
     settings.extra_groups = [
       "NormalFloat"
       "FloatBorder"
