bd9430a898b716682a1508da6cdcf6dbb2c64f6a
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-10-31 15:05:28 +0100

    (feat): (plugins) disabled java, conform full plugin structure with most recent nixvim

--- plugins/languages/java.nix
+++ plugins/languages/java.nix
@@ -3,13 +3,12 @@ _:
 # dunno where / how to do that without explicit control?
 {
   rootOpts.plugins = {
-    nvim-jdtls = {
-      enable = true;
+    jdtls = {
+      enable = false;
       # WARN: this expects that the project is directly inside of its workspace
       # folder: ..workspace/project.., if not like this then data will point
       # to the wrong place, and server will likely not run correctly.
-      data = "../.";
-      settings.java.gradle.enabled = true;
+      settings.settings.java.gradle.enabled = true;
     };
   };
 }
--- plugins/ui/wilder.nix
+++ plugins/ui/wilder.nix
@@ -1,40 +1,44 @@
-_: {
+{lib, ...}: {
   opts = {
     enable = true;
-    enableCmdlineEnter = true;
-    beforeCursor = true;
-    modes = ["/" "?" ":"];
+    settings = {
+      enable_cmdline_enter = true;
+      modes = ["/" "?" ":"];
+    };
 
-    renderer = ''
-      wilder.wildmenu_renderer(
-        wilder.wildmenu_lightline_theme({
-          highlights = {
-            gradient = (function()
-              local colors = {
-                '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
-                '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
-                '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
-                '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
-              }
-              local gradient = {}
-              for i, fg in ipairs(colors) do
-                  gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {
-                      { a = 1 },
-                      { a = 1 },
-                      { foreground = fg },
-                  })
-              end
-              return gradient
-            end)(),
-            separator = 'WilderSeparator',
-            default = 'WilderDefault',
-          },
-          highlighter = wilder.highlighter_with_gradient({
-            wilder.basic_highlighter(),
-          }),
-          separator = ' | ',
-        })
-      )
-    '';
+    options = {
+      before_cursor = true;
+      renderer = lib.nixvim.mkRaw ''
+        wilder.wildmenu_renderer(
+          wilder.wildmenu_lightline_theme({
+            highlights = {
+              gradient = (function()
+                local colors = {
+                  '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
+                  '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
+                  '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
+                  '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+                }
+                local gradient = {}
+                for i, fg in ipairs(colors) do
+                    gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {
+                        { a = 1 },
+                        { a = 1 },
+                        { foreground = fg },
+                    })
+                end
+                return gradient
+              end)(),
+              separator = 'WilderSeparator',
+              default = 'WilderDefault',
+            },
+            highlighter = wilder.highlighter_with_gradient({
+              wilder.basic_highlighter(),
+            }),
+            separator = ' | ',
+          })
+        )
+      '';
+    };
   };
 }
