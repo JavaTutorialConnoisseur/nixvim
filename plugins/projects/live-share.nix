1f557e3f350d81ff2f499861d463d1a3174116bb
JavaTutorialConnoisseur <parryfeline@proton.me>
2025-11-20 00:34:24 +0100

    (feat): swapped from pure instant.nix to live-share w/ ngrok

--- plugins/projects/live-share.nix
+++ plugins/projects/live-share.nix
@@ -1,3 +1,16 @@
 {pkgs, ...}: {
-  extra.packages = [pkgs.vimPlugins.live-share-nvim];
+  extra.packages = [pkgs.vimPlugins.live-share-nvim pkgs.vimPlugins.instant-nvim];
+  # this is necessary but UNFREE!!!
+  # rootOpts.extraPackages = [pkgs.ngrok];
+
+  # TEST: -> check if this works (on Friday)
+  extra.luaPre = ''
+    vim.g.instant_username = "<anon>"
+    require("live-share").setup({
+      port_internal = 8080,
+      max_attempts = 40,
+      service = "ngrok",
+      service_url = "/tmp/ngrok-url"
+    })
+  '';
 }
