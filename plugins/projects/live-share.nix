dedb7f8696b9fb52d7f166812a30d57a1acd0d24
JavaTutorialConnoisseur <parry@parrycat.org>
2025-12-04 06:46:59 +0100

    fix(plugins.projects): disabled ngrok temporarily

--- plugins/projects/live-share.nix
+++ plugins/projects/live-share.nix
@@ -3,6 +3,7 @@
   # this is necessary (to host) but UNFREE!!! Also needs your goddamn CREDIT CARD dude.
   # root.extraPackages = [pkgs.ngrok];
 
+  # require("live-share").setup({ port_internal = 8080, max_attempts = 40, service = "ngrok", service_url = "/tmp/ngrok-url" })
   # TEST: -> check if this works (on Friday)
   root.extraConfigLua = ''
     vim.g.instant_username = "<anon>"
